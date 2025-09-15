59208a2173d7dd9d82e99a93be59bad604f76203
ra1z <purnamaanrai7@gmail.com>
2025-09-15 13:39:38 +0545

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Purnaman Rai (College) on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
